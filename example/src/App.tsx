ea6fc29780a8c899d82ec5d5e99b38ac790a7fcf
anthonyjeamme <anthony@jeamme.fr>
2021-04-27 01:14:18 +0200

    example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,114 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+// import { TFormEvent } from '../../hooks/useForm/useForm.types'
+import { useForm, numberField, textField } from 'rocket-useform'
 
-import { useForm } from 'rocket-useform'
+const data = {}
 
-const App = () => {
-  console.log(useForm)
-  return <div>Hello world</div>
+const schema = {
+  user: {
+    firstName: textField({
+      required: true
+    }),
+    lastName: textField({
+      required: true
+    }),
+    age: numberField({
+      required: true
+    })
+  }
 }
 
-export default App
+const BasicExample = () => {
+  const refreshCounter = useRef<number>(0)
+  const form = useForm(data, schema)
+  const textareaRef = useRef<HTMLTextAreaElement>()
+
+  useEffect(() => {
+    const handleChange = (event: any) => {
+      console.log(event)
+      // @ts-ignore
+      textareaRef.current.value = JSON.stringify(form.toJSON(), null, 2)
+    }
+
+    form.addEventListener('change', handleChange)
+    return () => {
+      form.removeEventListener('change', handleChange)
+    }
+  }, [])
+
+  const handleSave = () => {
+    if (form.checkForm()) {
+      form.setModified(false)
+    } else {
+      console.log(`There is error(s) in form`)
+    }
+  }
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        padding: 40
+      }}
+    >
+      <div style={{ flex: 1 }}>
+        <div>Refresh counter : {refreshCounter.current++}</div>
+
+        <div>
+          <Input
+            placeholder='firstName'
+            {...matchCustomInput(form.get('user.firstName'))}
+          />
+          <Input
+            placeholder='lastName'
+            {...matchCustomInput(form.get('user.lastName'))}
+          />
+          <Input
+            type='number'
+            placeholder='age'
+            {...matchCustomInput(form.get('user.age'))}
+          />
+        </div>
+
+        <div>
+          <button disabled={!form.modified} onClick={handleSave}>
+            Save
+          </button>
+        </div>
+      </div>
+      <div style={{ flex: 1, position: 'relative' }}>
+        <textarea
+          ref={textareaRef}
+          style={{
+            width: '100%',
+            height: 500,
+            fontSize: 12
+          }}
+          readOnly
+          defaultValue={JSON.stringify(form.toJSON(), null, 2)}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default BasicExample
+
+const Input = ({ value, onChange, error, ...props }: any) => (
+  <input
+    {...props}
+    defaultValue={value}
+    onChange={(e) => {
+      onChange(e.target.value)
+    }}
+    style={{
+      border: error ? `1px solid red` : `1px solid #dddddd`
+    }}
+  />
+)
+
+export const matchCustomInput = ({ value, update, error }: any) => ({
+  value,
+  onChange: update,
+  error
+})
