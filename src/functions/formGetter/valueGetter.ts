806986ea8cd6fae4a3326ac1813484bbbff5c276
tony <anthony@jeamme.fr>
2021-05-17 04:25:41 +0200

    fix bug when data=null

--- src/functions/formGetter/valueGetter.ts
+++ src/functions/formGetter/valueGetter.ts
@@ -1,85 +1,85 @@
 import {
-	TFormDataValueFieldType,
-	TFormDataValueNode,
-	TFormValueGetter,
-	TValueFormSchemaNode
+  TFormDataValueFieldType,
+  TFormDataValueNode,
+  TFormValueGetter,
+  TValueFormSchemaNode
 } from '../../useForm.types'
 
 import {
-	getObjectPathChild,
-	pathToArrayPath,
-	pathToStringPath
+  getObjectPathChild,
+  pathToArrayPath,
+  pathToStringPath
 } from '../../utils/formPath'
 import { getGetterGenericFields, getStandardGetters } from './formGetter.common'
 import { TGetterProps } from './formGetter.types'
 
 const parseValue = (value: any, type: TFormDataValueFieldType) => {
-	switch (type) {
-		case 'string':
-			return value.toString()
-		case 'number':
-			return parseFloat(value)
-		case 'boolean':
-			return !!value
-	}
+  switch (type) {
+    case 'string':
+      return value?.toString?.()
+    case 'number':
+      return parseFloat(value)
+    case 'boolean':
+      return !!value
+  }
 }
 
 const valueGetter = ({
-	formDataRef,
-	path,
-	formTools,
-	formParams
+  formDataRef,
+  path,
+  formTools,
+  formParams
 }: TGetterProps): TFormValueGetter => {
-	const child = getObjectPathChild(formDataRef.current, path, formParams)
-	if (child.__node !== 'value') {
-		console.error(`${pathToStringPath(path)} is not a value`)
-		return null
-	}
+  const child = getObjectPathChild(formDataRef.current, path, formParams)
+  if (child.__node !== 'value') {
+    console.error(`${pathToStringPath(path)} is not a value`)
+    return null
+  }
 
-	const update = (value, refresh = false) => {
-		const node = getObjectPathChild(
-			formDataRef.current,
-			path,
-			formParams
-		) as TFormDataValueNode
+  const update = (value, refresh = false) => {
+    const node = getObjectPathChild(
+      formDataRef.current,
+      path,
+      formParams
+    ) as TFormDataValueNode
 
-		const params = (node.__schema as TValueFormSchemaNode).__params
+    const params = (node.__schema as TValueFormSchemaNode).__params
 
-		if (params.readOnly) {
-			console.error(`Can't write ${pathToStringPath(path)} : readOnly field`)
-			return
-		}
+    if (params.readOnly) {
+      console.error(`Can't write ${pathToStringPath(path)} : readOnly field`)
+      return
+    }
 
-		// TODO check entry value ?
-		const oldValue = node.__value
+    // TODO check entry value ?
+    const oldValue = node.__value
 
-		let needRefresh = false
+    let needRefresh = false
 
-		if (node.__error) {
-			node.__error = false
-			needRefresh = true
-		}
+    if (node.__error) {
+      node.__error = false
+      needRefresh = true
+    }
 
-		node.__value = parseValue(value, node.__type)
-		formTools.handleModified({
-			path: pathToArrayPath(path),
-			oldValue,
-			newValue: node.__value
-		})
-		if (needRefresh || refresh || params.autoRefresh) formTools.refresh()
-	}
+    node.__value = parseValue(value, node.__type)
+    formTools.handleModified({
+      path: pathToArrayPath(path),
+      oldValue,
+      newValue: node.__value
+    })
+    if (needRefresh || refresh || params.autoRefresh) formTools.refresh()
+  }
 
-	return {
-		...getGetterGenericFields({ formDataRef, child, path, formParams }),
-		...getStandardGetters({
-			formDataRef,
-			formParams,
-			formTools,
-			path
-		}),
-		value: child.__value,
-		update
-	}
+  return {
+    ...getGetterGenericFields({ formDataRef, child, path, formParams }),
+    ...getStandardGetters({
+      formDataRef,
+      formParams,
+      formTools,
+      path
+    }),
+    value: child.__value,
+    update
+  }
 }
 
 export default valueGetter
